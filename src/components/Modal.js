046f0f41906758d8592d6ab85fcd6ef8215b1cb3
Tolulope Olaniyan <tolulopeolaniyan@Tolulopes-MacBook-Pro.local>
2020-02-01 15:51:19 +0100

    Improve UI

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -26,7 +26,7 @@ export default class Modal extends Component {
                       <h5>Item added to Cart</h5>
                       <img src={img} className="img-fluid" alt="product" />
                       <h5>{title}</h5>
-                      <h5 className="text-muted">price : $ {price}</h5>
+                      <h5 className="text-muted">price : NGN {price}</h5>
                       <Link to="/">
                         <ButtonContainer cart onClick={() => closeModal()}>
                           store
